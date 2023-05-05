1753d1d8d88210faca9c838d759762b989a327d7
Shakiba-G <69167910+Shakiba-G@users.noreply.github.com>
2023-05-05 05:41:11 +0330

    Added daily weather forecast

--- index.js
+++ index.js
@@ -34,7 +34,50 @@ let months = [
   return `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} | ${currentHours}:${currentMinutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+  ];
+  return days[day];
+}
+
+function displayForecast(response) {
+let forecastElement = document.querySelector("#forecast");
+let forecast = response.data.daily;
+let forcastHTML = `<div class="row">`;
+forecast.forEach(function (forecastDay, index) {
+  if (index < 6) {
+  forcastHTML = forcastHTML +
+  `<div class="col-2">
+    <div class="forecast-day">${formatDay(forecastDay.time)}</div>
+    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png" alt="" width="70" />
+    <div class="forecast-temps">
+      <span class="forecast-temp-max">
+      ${Math.round(forecastDay.temperature.maximum)}° </span>
+      <span class="forecast-temp-min">
+      ${Math.round(forecastDay.temperature.minimum)}° </span>
+    </div>
+  </div>`;
+  }
+});
+  forcastHTML = forcastHTML + `</div>`;
+  forecastElement.innerHTML = forcastHTML;
+  }
   
+function getForecast(coordinates) {
+  let apiKey = "05ff88a47te358f7cf0o768f24abc234";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
@@ -62,6 +105,7 @@ function displayWeather(response) {
   document.querySelector("#time").innerHTML = formatDate(response.data.dt);
   document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   document.querySelector("#icon").setAttribute("alt",response.data.weather[0].main);
+  getForecast(response.data.coord);
 }
   
 function findCity(city) {
@@ -76,8 +120,6 @@ function enterCity(event) {
   let city = cityInput.toLowerCase();
   findCity(city);
 }
-
-findCity("New York");
 
 let searchButton = document.querySelector("#search-form");
 searchButton.addEventListener("submit", enterCity); 
@@ -117,4 +159,7 @@ function getCurrentLocation(event) {
 }
   
 let currentButton = document.querySelector("#current-button");
-  currentButton.addEventListener("click", getCurrentLocation);
+currentButton.addEventListener("click", getCurrentLocation);
+
+findCity("New York");
+displayForecast();
